968d36c1004d495c9f79d044ee67d3d19034c60c
niconico620 <nicolas.andre.pf@gmail.com>
2020-07-06 04:03:14 +0800

    first commit

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,7 @@
-module.exports = {
+/* module.exports = {
     mongoURI:process.env.MONGO_URI
+}
+ */
+module.exports = {
+    mongoURI: 'mongodb+srv://niconico620:admin@cluster0-gozjd.gcp.mongodb.net/MovieDB?retryWrites=true&w=majority'
 }
