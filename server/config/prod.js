7863f59f6aaa7ea91736902b7899ae7f28c66608
niconico620 <nicolas.andre.pf@gmail.com>
2020-07-06 04:27:14 +0800

    production build

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,7 +1,3 @@
-/* module.exports = {
+ module.exports = {
     mongoURI:process.env.MONGO_URI
-}
- */
-module.exports = {
-    mongoURI: 'mongodb+srv://niconico620:admin@cluster0-gozjd.gcp.mongodb.net/MovieDB?retryWrites=true&w=majority'
-}
+ }
